3f0e568045ee71a63e38b19ae6379dc240fc4d3c
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-08-03 23:27:06 +0530

    v1.1

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
+    int a=5,c=6,b=0;
+    b=a+c;
     cout << "Hello Saswat!!" << endl;
+    cout << "Output of the Addition Operation:" << b;
+    bool ai = true;
+    cout << ai;
     return 0;
 }
