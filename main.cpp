a21fdac235dfccb96bf1ef37b24ad5ed57ce6e1e
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-08-06 15:52:56 +0530

    Variables
    int,float,double,bool,string,char

--- main.cpp
+++ main.cpp
@@ -7,8 +7,28 @@ int main()
     int a=5,c=6,b=0;
     b=a+c;
     cout << "Hello Saswat!!" << endl;
-    cout << "Output of the Addition Operation:" << b;
+    cout << "Output of the Addition Operation:" << b <<endl;
     bool ai = true;
-    cout << ai;
+    cout << ai << endl;
+
+    char d='x';             //->1 Byte
+    cout << d<< endl;
+
+    double dob=82496.96287;
+     string st="Hello this is a string variable.>!..";
+    cout << st << endl;
+    //To Know the size ..!
+    int size=sizeof(st);
+    cout<<"Size of String is:"<<size<<endl;
+
+
+    float f=1.23;
+    cout<<"Size of Float is:"<<sizeof(f)<<endl;
+    cout<<"Size of Int is:"<<sizeof(a)<<endl;
+    cout<<"Size of Char is:"<<sizeof(d)<<endl;
+    cout<<"Size of Boolean is:"<<sizeof(ai)<<endl;
+    cout<<"Size of Double is:"<<sizeof(dob)<<endl;
+
+
     return 0;
 }
