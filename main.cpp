fc942263af848802b5e76fef1af1f07b1158c74c
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-08-06 16:04:48 +0530

    taking Input from user

--- main.cpp
+++ main.cpp
@@ -29,6 +29,23 @@ int main()
     cout<<"Size of Boolean is:"<<sizeof(ai)<<endl;
     cout<<"Size of Double is:"<<sizeof(dob)<<endl;
 
+    //Class-2
+    //Take Input from the User..!
+    int inp1;
+    cout<<"Enter a Number to be executed"<<endl;
+    cin>>inp1;
+    cout<<"The Input was "<<inp1<<endl;
+
+    //Take Input But this time take two Inp values
+    int inp2,inp3;
+    cin>> inp2 >> inp3;
+    cout<<"The Input Values of inp2 and inp3 is "<<inp2 <<"\t"<<inp3<<endl;
+
+
+    //Illustration of garbage value..!
+    int k;
+    cout<<"Garbage value in K is-->"<<k;
+
 
     return 0;
 }
