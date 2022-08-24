8942b522150f4a5a2f07591b886c4a894a0b793c
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-08-25 00:57:13 +0530

    two_sorted

--- README.md
+++ README.md
@@ -56,6 +56,7 @@ In any large system, you typically find C++ in the lower-level and performance-c
 🌟Selection Sort- **https://gist.github.com/geeky-auro/0a8750b424a38235805a8efbe3e26f52** <br>
 🌟Bubble Sort- **https://gist.github.com/geeky-auro/4083081f2fe43266e0956cce343f166c**  <br>
 🌟Insetion Sort- **https://gist.github.com/geeky-auro/dee397206eda15c2ba67dc4c70b7648a** <br>
+🌟Code Merge Two Sorted Arrays- **https://gist.github.com/geeky-auro/2f1523aaef82a921a47f92d73c301a27** <br>
 
 ## Important Coding Questions:-
 🌟Second Largest- **https://gist.github.com/geeky-auro/179b7af8535bf640524d90c3f39320cc** <br>
--- sorting/main.cpp
+++ sorting/main.cpp
@@ -53,6 +53,66 @@ void insertionSort(int *arr,int n){
 
 }
 
+void optimizedInsertionSort(int *arr, int n){
+
+for(int i=1;i<n;i++){
+        int current=arr[i],j;
+    for(j=i-1;j>=0;j--){
+        if(current<arr[j]){
+            arr[j+1]=arr[j];
+        }
+        else{
+            break;
+        }
+    }
+    arr[j+1]=current;
+}
+
+}
+
+void merge(int *arr1, int size1, int *arr2, int size2, int *ans)
+{
+    //Write your code here
+    int counter_i=0,counter_j=0,ans_index=0,i;
+    for( i=0;i<(size1+size2);i++){
+        if(arr1[counter_i]<arr2[counter_j] && counter_i<size1){
+            ans[i]=arr1[counter_i];
+            counter_i++;
+            continue;
+        }
+        else if(counter_j<size2){
+            ans[i]=arr2[counter_j];
+            counter_j++;
+            continue;
+    }
+
+    while(counter_i<size1 && i<size1+size2){
+        ans[i++]=arr1[counter_i];
+        counter_i++;
+    }
+    while(counter_j<size2 && i<size1+size2){
+                ans[i++]=arr2[counter_j];
+        counter_j++;
+    }
+
+    break;
+/*
+    for(int j=counter_i,k=ans_index;j<size1 && k<(size1+size2);j++,k++){
+        ans[ans_index]=arr1[j];
+    }
+    for(int j=counter_j,k=ans_index;j<size2 && k<(size1+size2); j++,k++){
+         ans[ans_index]=arr2[j];
+    }
+    */
+
+
+}
+}
+
+
+
+
+
 void printArray(int *arr,int n){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<"\t";
@@ -62,16 +122,26 @@ void printArray(int *arr,int n){
 int main()
 {
     cout<<"Enter the size of the array"<<endl;
-    int n;
+    int n,m;
+    cin>>m;
     cin>>n;
-    int *arr=new int[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    int *arr1=new int[m];
+    for(int i=0;i<m;i++){
+        cin>>arr1[i];
     }
+
+    int *arr2=new int[n];
+    for(int i=0;i<n;i++){
+        cin>>arr2[i];
+    }
+    int *ans=new int[m+n];
+
  //   selectionSort(arr,n);
  //   bubblesort(arr,n);
-      insertionSort(arr,n);
-    printArray(arr,n);
+ //     insertionSort(arr,n);
+ //     optimizedInsertionSort(arr,n);
+ merge(arr1,m,arr2,n,ans);
+    printArray(ans,m+n);
 
     return 0;
 }
