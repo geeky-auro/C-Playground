75f14591bf8cb82224ba6fc85ff1b8d0276e5fc6
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-09-22 22:46:27 +0530

    important Macros

--- Temporary Playground/macros_constant.cpp
+++ Temporary Playground/macros_constant.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 #define PI 3.14
+
+#define MULTIPLY(a,b)  (a*b);
 // Defining Global Variables..!
 int a=10;
 
@@ -15,7 +17,22 @@ void incre_a(){
 }
 
 int main(){
+    //THIS defined function will treat as string 
+    /*
+    "MULTIPLY(a, b)" will be replaced by "a*b" in code.
+    So, MULTIPLY(2+3, 3+5) will be replaced by 2+3*3+5. 
+    And according to operator precedence, multiply operator (*) has higher precedence than plus operator (+). 
+    So, 3*3 will be evaluated first. Hence expression will become :
+    2+9+5 = 16
+    **/
+    cout<<"Multiplication of Defined Function is "<< MULTIPLY(3+2,5+3);
+    cout<<endl;
     int r=5;
+    {
+        int a=10;
+        cout<<a<<endl;
+        incre_a1(a);
+    }
     incre_a1(a);
     cout<<a<<endl;
     incre_a();
