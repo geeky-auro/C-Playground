86a3a0c21f770eedd3947820ffa040d8cf3f3d48
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-09-24 19:50:32 +0530

    const Questions

---
+++ Temporary Playground/const_questions.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int p=5;
+    const int *q=&p;
+    /// @brief Here you cannot change the value of the variable pointed by the pointer
+    /// @return 
+    int j=10;
+    int * const q=&j;
+    /// @brief Here you cannot update the address of the pointer
+    /// @return 
+    int k=10;
+    int const * const q=&k;
+    /// @brief Here you cannot update the address as well as value of the variable pointed by the pointer 
+    /// @return 
+    return 0;
+}
