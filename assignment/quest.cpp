9bd8ceec70c2de7c70113e86c67845e9dcb83c23
Harsh <harshprasad907@gmail.com>
2022-10-11 00:55:22 +0530

    Constructor

--- assignment/quest.cpp
+++ assignment/quest.cpp
@@ -20,8 +20,5 @@ int main(){
     static double i;
     // Static variables (like global variables) 
     // are initialized as 0 if not initialized explicitly. For example in the below program, value of x is printed as 0, while value of y is something garbage
-    Test t1,t2(&i);
-    t1.display();
-    t2.display1();
     return 0;
 }
