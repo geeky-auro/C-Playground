e39200d5945314a166d0ea60d43d6103ec67d846
geeky-auro <aurosaswat.apk@gmail.com>
2023-08-06 11:34:24 +0530

    Updated DAA lab assignment 1

--- DAA LAB/DAY-2/prog1.c
+++ DAA LAB/DAY-2/prog1.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+
+int powi(int n,int m){
+    int ans=1;
+    for(int i=1;i<=m;i++){
+        ans=ans*n;
+    }
+    return ans;
+}
+
+int dec_to_bin(int no){
+    int rem=0,dig=0,count=0;
+    while(no>=1){
+        rem=no%2;
+        dig=dig+powi(10,count++)*rem;
+        no=no/2;
+    }
+    return dig;
+}
 
 int main(){
     
@@ -26,7 +45,7 @@ int main(){
         // Write your function to compute on numbers
         // solve()
 
-        fprintf(outputFile,"%.2lf\n",result);
+        fprintf(outputFile,"%.32d\n",dec_to_bin(number));
     }
 
     fclose(inputFile);
